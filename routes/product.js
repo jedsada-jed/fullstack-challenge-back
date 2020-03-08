2219e9f27ed6bb055a211c95e7cc052153358d22
JED <jedsada.ngow@gmail.com>
2020-03-08 18:29:07 +0700

    full loop success

--- routes/product.js
+++ routes/product.js
@@ -5,31 +5,75 @@ const product = require('../models/product')
 const checkAuth = require('../config/passport')
 
 router.get('/', checkAuth, async (req, res, next) => {
-  const data = await product.findAll()
-  res.json(data);
+  try {
+    const data = await product.findAll()
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
 });
 
 router.get('/:id', async (req, res, next) => {
-  const data = await product.findOne({
-    where: {
-      id: req.params.id
-    },
-  });
-  res.json(data);
+  try {
+    const data = await product.findOne({
+      where: {
+        id: req.params.id
+      },
+    });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
 });
 
-// TODO
 router.post('/', async (req, res, next) => {
-  const { title, description, price } = req.body
-  const data = await product.create({
-    title,
-    description,
-    price,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }, null);
+  try {
+    const { title, description, price } = req.body
+    const data = await product.create({
+      title,
+      description,
+      price,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+});
 
-  res.json(data);
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { title, description, price } = req.body
+    const data = await product.upsert({
+      id: req.params.id,
+      title,
+      description,
+      price,
+      updatedAt: new Date()
+    });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const res = await product.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    res.json({ data: res == 1 });
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
 });
 
 
