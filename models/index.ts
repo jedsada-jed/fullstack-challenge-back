0eb1d6652176cf619cb5d21e7bc367778621c002
JED <jedsada.ngow@gmail.com>
2020-03-12 00:24:05 +0700

    create sqlite db when env = dev

--- models/index.ts
+++ models/index.ts
@@ -3,6 +3,13 @@ import { Sequelize } from "sequelize"
 const env = process.env.NODE_ENV || "development"
 const config = require("../config/config.json")[env]
 
+if (env == 'development' &&
+  config.development &&
+  config.development.dialect == 'sqlite') {
+  const sqlite = require('sqlite3');
+  new sqlite.Database(config.development.storage);
+}
+
 export default new Sequelize({
   ...config,
   pool: {
